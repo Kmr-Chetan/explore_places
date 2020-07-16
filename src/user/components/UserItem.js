48959be5830e171c8f31205f2a3b75c952fc90d0
chetan kumar <ckumarhcu@gmail.com>
2020-07-17 00:47:39 +0530

    sideDrawer

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Avatar from "../../shared/components/UIElements/Avatar";
-import Card from "../../shared/components/UIElements/Card";
-import { Link } from "react-router-dom";
-import "./UserItem.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
+import './UserItem.css';
 
-const UserIteam = (props) => {
+const UserItem = props => {
   return (
     <li className="user-item">
       <Card className="user-item__content">
@@ -16,7 +16,7 @@ const UserIteam = (props) => {
           <div className="user-item__info">
             <h2>{props.name}</h2>
             <h3>
-              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
             </h3>
           </div>
         </Link>
@@ -25,5 +25,4 @@ const UserIteam = (props) => {
   );
 };
 
-export default UserIteam;
-
+export default UserItem;
